109a71eafa72381eac78fe10a3e208b969392923
pavan <pavannttf29@gmail.com>
2021-03-26 11:16:05 +0530

    implemented private route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ import Login from './componets/Login';
 import About from './Pages/About';
 import Card from './componets/Card';
 import Cart from './Pages/Cart';
+import {useSelector} from 'react-redux';
+
+// protected route
+function PrivateRoute({children}) {
+  let auth = useSelector (state => state.auth.login);
+  console.log ('app', auth);
+  if (auth) {
+    return children;
+  } else {
+    return <Redirect to="/login" />;
+  }
+}
 
 function App () {
   return (
@@ -19,7 +31,7 @@ function App () {
         <Route exact path="/" component={Card} />;
         <Route exact path="/login" component={Login} />
         <Route exact path="/about" component={About} />;
-        <Route exact path="/cart" component={Cart} />;
+        <PrivateRoute exact path="/cart" component={Cart} />;
       </Switch>
     </Router>
   );
--- src/redux/reducer/authReducer.js
+++ src/redux/reducer/authReducer.js
@@ -1,6 +1,6 @@
 export const authReducer = (
   state = {
-    login: true,
+    login: false,
   },
   action
 ) => {
